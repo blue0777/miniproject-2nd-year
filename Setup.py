f956d35ad619938c9bc2fbec3f3a70c1bb83f13b
Cosmic <125358161+blue0777@users.noreply.github.com>
2023-04-25 15:18:29 +0530

    Create Setup.py

---
+++ Setup.py
@@ -0,0 +1,7 @@
+class guu:
+  openai_api_key ='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'
+  bot_token = '6274606987:AAGyt2CbaBT9ILOvUp7wvvMQDaSHGKY2Q-k'
+  api_hash = '316c4522261f8b1ff63ba1b875837d89'
+  api_id = '9567537'
+  model = "text-davinci-003" 
+  mxtoken = 1024
