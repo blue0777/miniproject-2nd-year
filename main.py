186fdd890ef6367f788e74062ad9aac6077665f6
Cosmic <125358161+blue0777@users.noreply.github.com>
2023-05-01 16:14:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 import asyncio
-from Setup import guu
+import os
 import openai
 from pyrogram import filters, Client
-from textblob import TextBlob
-import pymongo
+import sqlite3
 
-# Set up MongoDB client and database
-client = pymongo.MongoClient("mongodb+srv://cosmic:Soham3112@feed.gmzicww.mongodb.net/?retryWrites=true&w=majority")
-db = client["feedback_collection"]
-feedback_collection = db["Feedback"]
 
-Soham = Client(name="OpenaiBot",api_id=guu.api_id,api_hash=guu.api_hash)
+# Set up SQLite database
+conn = sqlite3.connect('feedback.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS Feedback
+             (id INTEGER PRIMARY KEY AUTOINCREMENT,
+              user_id INTEGER,
+              username TEXT,
+              feedback TEXT)''')
+conn.commit()
+
+
+Soham = Client(name="OpenaiBot", api_id=os.environ.get("API_ID"), api_hash=os.environ.get("API_HASH"))
+
 
 async def ai(query):
-    openai.api_key = guu.openai_api_key
-    completion = openai.Completion.create(engine=guu.model, prompt=query, max_tokens=guu.mxtoken, n=1, stop=None,temperature=0.7)
+    openai.api_key = os.environ.get("OPENAI_API_KEY")
+    completion = openai.Completion.create(engine=os.environ.get("MODEL"), prompt=query, max_tokens=int(os.environ.get("MAX_TOKENS")), n=1, stop=None,temperature=0.7)
     result = completion.choices[0].text
     return result
 
-def get_sentiment(text):
-    analysis = TextBlob(text)
-    return analysis.sentiment.polarity
 
 @Soham.on_message(filters.command("feedback") & ~filters.group)
 async def feedback_handler(bot, msg):
@@ -29,16 +33,24 @@ async def feedback_handler(bot, msg):
     user_id = msg.from_user.id
     username = msg.from_user.username
 
-    feedback_collection.insert_one({
-        "user_id": user_id,
-        "username": username,
-        "feedback": feedback
-    })
+    c.execute("INSERT INTO Feedback (user_id, username, feedback) VALUES (?, ?, ?)", (user_id, username, feedback))
+    conn.commit()
+
+    chat = await bot.get_chat("cosmic98")
+    chat_id = chat.id
+
+    await bot.send_message(
+        chat_id=chat_id,
+        text=f"New feedback from @{username} (user ID: {user_id}):\n{feedback}"
+    )
 
     await bot.send_message(
         chat_id=user_id,
         text="Thank you for your feedback! We appreciate your response 🙂."
     )
+
+
+
 
 
 @Soham.on_message(filters.command("start") & ~filters.group)
@@ -52,30 +64,22 @@ async def main(bot,msg):
                     'Feel free to ask me anything! ☺️\n\n'
                     'MADE BY : Bluecake  \n'
                     'Git-Hub Profile : https://github.com/blue0777\n'
-                    'Git-Hub Reposotory : http://github.com/blue0777/TG-ChatGPT-Bot\n'
-                    'Please leave your feedback @cosmic_89_bot')
+                    'Git-Hub Repository : http://github.com/blue0777/TG-ChatGPT-Bot\n'
+                    )
     DEL = await msg.reply(f"Typing🤔.......")
     await asyncio.sleep(3)
     await DEL.delete(10)
+
 
 @Soham.on_message(filters.text & ~filters.group)
 async def main(bot, msg):
     newbie = msg.from_user.id
     ques = msg.text
-    sentiment_score = get_sentiment(ques)
-    if sentiment_score < -0.5:
-        expression = "😔"
-    elif sentiment_score < 0:
-        expression = "😕"
-    elif sentiment_score == 0:
-        expression = "😐"
-    elif sentiment_score < 0.5:
-        expression = "🙂"
-    else:
-        expression = "😄"
+    print(ques)
     guu = await ai(ques)
     await asyncio.sleep(3)
-    test = f"`{expression} {guu}`"
+    print(guu)
+    test = f"`{guu}`"
     await asyncio.sleep(1)
     await bot.send_message(newbie,test)
 
