b172f364dd826bf37b4a236c2f60778eddae9c8a
Cosmic <125358161+blue0777@users.noreply.github.com>
2023-05-02 08:45:21 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 class guu:
-  openai_api_key ='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'
+  openai_api_key ='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'
   bot_token = '6274606987:AAGyt2CbaBT9ILOvUp7wvvMQDaSHGKY2Q-k'
   api_hash = '316c4522261f8b1ff63ba1b875837d89'
   api_id = '9567537'
